b443f798e5d1d8a58a807974a5879d9c3813750b
lamooon <111581657+lamooon@users.noreply.github.com>
2023-12-08 19:50:56 +0900

    Update knexfile.js

--- app/api-server/knexfile.js
+++ app/api-server/knexfile.js
@@ -9,7 +9,7 @@ module.exports = {
       host: 'mysql',
       // Environment variables are defined in the api service in docker-compose
       user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
+      password: process.env.MYSQL_ROOT_PASSWORD,
       database: process.env.MYSQL_DATABASE,
       charset: 'utf8',
     }
